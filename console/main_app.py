468295d681f5c846ade4df1ca85b180b44b97285
TalhaEryasar <talhaeryasar@gmail.com>
2020-01-25 01:52:34 +0000

    key_event_handler function updated for both left_right and up_right keys

--- console/main_app.py
+++ console/main_app.py
@@ -36,11 +36,12 @@ class CursBaseDialog:
         self.message = options.get('message', '')
         self.win.addstr(0, 0, ' ' * self.maxx, self.title_attr)
         self.win.keypad(True)
-        self.focus = 0
+        # self.focus = 0
+        self.focus = [0, 0]
         self.enterKey = False
         self.win.keypad(True)
         self.menu = ['Info', 'Convert', 'Analyse', 'Visualise', 'Exit']
-        # self.options = ('Yes   ', 'Cancel')
+        self.option = ('Yes   ', 'Cancel')
         
         # print title
         if self.title:
@@ -55,13 +56,18 @@ class CursBaseDialog:
         curses.noecho()
         curses.cbreak()
 
-    def left_right_key_event_handler(self, max):
+    def key_event_handler(self):
+        max = len(self.option)
         self.win.refresh()
         key = self.win.getch()
-        if key == curses.KEY_LEFT and self.focus != 0:
-            self.focus -= 1
-        elif key == curses.KEY_RIGHT and self.focus != max - 1:
-            self.focus += 1
+        if key == curses.KEY_LEFT and self.focus[0] != 0:
+            self.focus[0] -= 1
+        elif key == curses.KEY_RIGHT and self.focus[0] != max - 1:
+            self.focus[0] += 1
+        elif key == curses.KEY_UP and self.focus[1] != 0:
+            self.focus[1] -= 1
+        elif key == curses.KEY_DOWN and self.focus[1] != max - 1:
+            self.focus[1] += 1
         elif key == ord('\n'):
             self.enterKey = True
 
@@ -133,37 +139,39 @@ class ShowWelcomePage(CursBaseDialog):
     def show_welcome_page(self):
         while not self.enterKey:
             for idx, row in enumerate(self.menu):
-                if idx == self.focus:
+                if idx == self.focus[1]:
                     self.win.addstr(int(self.y / 2 + idx), int(self.x / 2 - len(row) // 2), row,
                                     self.opt_attr | self.focus_attr)
                 else:
                     self.win.addstr(int(self.y / 2 + idx), int(self.x / 2 - len(row) // 2), row, self.opt_attr)
+            self.welcome_key_event_handler()
 
-            self.win.refresh()
-            key = self.win.getch()
-
-            if key == curses.KEY_UP and self.focus != 0:
-                self.focus -= 1
-            elif key == curses.KEY_DOWN and self.focus != len(self.menu) - 1:
-                self.focus += 1
-            elif key == ord('\n'):
-                # if user selected "Exit", exit the program
-                if self.menu[self.focus] == 'Exit':
-                    sys.exit(0)
-                # if user wants intro, give it to him :D
-                elif self.menu[self.focus] == 'Info':
-                    show_info_page(title='Info Page')
-                    self.__init__(title='CI5235 Ethical Hacking')
-                elif self.menu[self.focus] == 'Convert':
-                    show_convert_page()
-                    self.__init__(title='CI5235 Ethical Hacking')
-                elif self.menu[self.focus] == 'Analyse':
-                    show_analyse_page()
-                    self.__init__(title='CI5235 Ethical Hacking')
-                elif self.menu[self.focus] == 'Visualise':
-                    self.enterKey = True
-                else:
-                    sys.exit(1)
+    def welcome_key_event_handler(self):
+        self.win.refresh()
+        key = self.win.getch()
+        
+        if key == curses.KEY_UP and self.focus[1] != 0:
+            self.focus[1] -= 1
+        elif key == curses.KEY_DOWN and self.focus[1] != len(self.menu) - 1:
+            self.focus[1] += 1
+        elif key == ord('\n'):
+            # if user selected "Exit", exit the program
+            if self.menu[self.focus[1]] == 'Exit':
+                sys.exit(0)
+            # if user wants intro, give it to him :D
+            elif self.menu[self.focus[1]] == 'Info':
+                show_info_page(title='Info Page')
+                self.__init__(title='CI5235 Ethical Hacking')
+            elif self.menu[self.focus[1]] == 'Convert':
+                show_convert_page()
+                self.__init__(title='CI5235 Ethical Hacking')
+            elif self.menu[self.focus[1]] == 'Analyse':
+                show_analyse_page()
+                self.__init__(title='CI5235 Ethical Hacking')
+            elif self.menu[self.focus[1]] == 'Visualise':
+                self.enterKey = True
+            else:
+                sys.exit(1)
 
 
 class ShowInfoPage(CursBaseDialog):
@@ -182,13 +190,13 @@ class ShowInfoPage(CursBaseDialog):
 
 class AskLogfileCreate(CursBaseDialog):
     def ask_logfile_create(self):
-        option = ('Yes   ', 'Cancel')
-        rectangle(self.win, int(self.maxy / 2) - 1, int(self.maxx / 2) - 13, 2, len(option[0]) + 1, self.opt_attr)
-        rectangle(self.win, int(self.maxy / 2) - 1, int(self.maxx / 2) + 2, 2, len(option[1]) + 1, self.opt_attr)
+        # self.option = ('Yes   ', 'Cancel')
+        rectangle(self.win, int(self.maxy / 2) - 1, int(self.maxx / 2) - 13, 2, len(self.option[0]) + 1, self.opt_attr)
+        rectangle(self.win, int(self.maxy / 2) - 1, int(self.maxx / 2) + 2, 2, len(self.option[1]) + 1, self.opt_attr)
         pos_x = [int(self.maxx / 2) - 13, int(self.maxx / 2) + 2]
 
         while not self.enterKey:
-            if self.focus == 0:
+            if self.focus[0] == 0:
                 self.win.addstr(int(self.maxy / 2), int(self.maxx / 2) - 12, ' Yes  ', self.focus_attr | self.opt_attr)
                 self.win.addstr(int(self.maxy / 2), int(self.maxx / 2) + 3, 'Cancel', self.opt_attr)
             else:
@@ -196,27 +204,27 @@ class AskLogfileCreate(CursBaseDialog):
                 self.win.addstr(int(self.maxy / 2), int(self.maxx / 2) + 3, 'Cancel', self.focus_attr | self.opt_attr)
 
             for i in range(2):
-                if i != self.focus:
-                    rectangle(self.win, int(self.maxy / 2)-1, pos_x[i], 2, len(option[i]) + 1,
+                if i != self.focus[0]:
+                    rectangle(self.win, int(self.maxy / 2)-1, pos_x[i], 2, len(self.option[i]) + 1,
                               curses.A_NORMAL | self.opt_attr)
                 else:
-                    rectangle(self.win, int(self.maxy / 2)-1, pos_x[self.focus], 2, len(option[self.focus]) + 1,
+                    rectangle(self.win, int(self.maxy / 2)-1, pos_x[self.focus[0]], 2, len(self.option[self.focus[0]]) + 1,
                               self.focus_attr | self.opt_attr)
-            self.left_right_key_event_handler(2)
-        if self.focus == 0:
+            self.key_event_handler()
+        if self.focus[0] == 0:
             return True
         return False
 
 
 class AskDeleteXmlFiles(CursBaseDialog):
     def ask_delete_xml_files(self):
-        option = ('Yes   ', 'Cancel')
-        rectangle(self.win, int(self.maxy / 2) - 1, int(self.maxx / 2) - 13, 2, len(option[0]) + 1, self.opt_attr)
-        rectangle(self.win, int(self.maxy / 2) - 1, int(self.maxx / 2) + 2, 2, len(option[1]) + 1, self.opt_attr)
+        # option = ('Yes   ', 'Cancel')
+        rectangle(self.win, int(self.maxy / 2) - 1, int(self.maxx / 2) - 13, 2, len(self.option[0]) + 1, self.opt_attr)
+        rectangle(self.win, int(self.maxy / 2) - 1, int(self.maxx / 2) + 2, 2, len(self.option[1]) + 1, self.opt_attr)
         pos_x = [int(self.maxx / 2) - 13, int(self.maxx / 2) + 2]
 
         while not self.enterKey:
-            if self.focus == 0:
+            if self.focus[0] == 0:
                 self.win.addstr(int(self.maxy / 2), int(self.maxx / 2) - 12, ' Yes  ', self.focus_attr | self.opt_attr)
                 self.win.addstr(int(self.maxy / 2), int(self.maxx / 2) + 3, 'Cancel', self.opt_attr)
             else:
@@ -224,13 +232,13 @@ class AskDeleteXmlFiles(CursBaseDialog):
                 self.win.addstr(int(self.maxy / 2), int(self.maxx / 2) + 3, 'Cancel', self.focus_attr | self.opt_attr)
 
             for i in range(2):
-                if i != self.focus:
-                    rectangle(self.win, int(self.maxy / 2) - 1, pos_x[i], 2, len(option[i]) + 1,
+                if i != self.focus[0]:
+                    rectangle(self.win, int(self.maxy / 2) - 1, pos_x[i], 2, len(self.option[i]) + 1,
                               curses.A_NORMAL | self.opt_attr)
                 else:
-                    rectangle(self.win, int(self.maxy / 2) - 1, pos_x[self.focus], 2, len(option[self.focus]) + 1,
+                    rectangle(self.win, int(self.maxy / 2) - 1, pos_x[self.focus[0]], 2, len(self.option[self.focus[0]]) + 1,
                               self.focus_attr | self.opt_attr)
-            self.left_right_key_event_handler(2)
+            self.key_event_handler()
         if self.focus == 0:
             return True
         return False
